3c96788ed585c13063094a479de6ba267cffc0ce
Bert1041 <ishokarobertt@gmail.com>
2022-06-06 18:04:45 +0100

    first commit

---
+++ resources/js/Pages/Posts/Home.vue
@@ -0,0 +1,66 @@
+<script setup>
+import BreezeAuthenticatedLayout from "@/Layouts/Authenticated.vue";
+import { Head } from "@inertiajs/inertia-vue3";
+const props = defineProps({
+    posts: Array
+});
+</script>
+
+<template>
+    <Head title="Posts" />
+
+    <BreezeAuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Home Page For All Posts
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
+                    <div class="p-6 bg-white border-b border-gray-200">
+                        <!-- <div class="mb-4">
+                            <Link
+                                class="px-6 py-2 mb-2 text-green-100 bg-green-500 rounded"
+                                :href="route('posts.create')"
+                            >
+                                Posts Create
+                            </Link>
+                        </div> -->
+                        <table>
+                            <thead class="font-bold bg-gray-300 border-b-2">
+                                <td class="px-4 py-2">ID</td>
+                                <td class="px-4 py-2">Title</td>
+                                <td class="px-4 py-2">Description</td>
+                                <td class="px-4 py-2">Action</td>
+                            </thead>
+                            <tbody>
+                                <tr v-for="post in posts" :key="post.id">
+                                    <td class="px-4 py-2">{{ post.id }}</td>
+                                    <td class="px-4 py-2">{{ post.title }}</td>
+                                    <td class="px-4 py-2">
+                                        {{ post.description }}
+                                    </td>
+                                    <td class="px-4 py-2 font-extrabold">
+                                        <!-- <Link
+                                            class="text-green-700"
+                                            :href="route('posts.edit', post.id)"
+                                        >
+                                            Edit
+                                        </Link>
+                                        <Link
+                                            @click="destroy(post.id)"
+                                            class="text-red-700"
+                                            >Delete</Link
+                                        > -->
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </BreezeAuthenticatedLayout>
+</template>
